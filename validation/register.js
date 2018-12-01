601172caed264b8a628d9c39596aa64909bf8b14
Elizabeth Rosso <lizprogrammer@yahoo.com>
2018-12-01 12:46:54 -0500

    completed validation

--- validation/register.js
+++ validation/register.js
@@ -10,22 +10,32 @@ module.exports = function validateRegisterInput(data) {
   data.password2 = !isEmpty(data.password2) ? data.password2 : '';
 
 
-
   if(!Validator.isLength(data.name, { min: 2, max: 30 })){
     errors.name = 'Name must be between 2 and 30 characters';
+    console.log(data.name);
   }
 
 if(Validator.isEmpty(data.name)){
   errors.name = 'Name field is required';
 }
+
 if(Validator.isEmpty(data.email)){
   errors.email = 'Email field is required';
 }
-if(Validator.isEmail(data.email)){
+if(!Validator.isEmail(data.email)){
   errors.email = 'Email is invalid';
 }
 if(Validator.isEmpty(data.password)){
-  errors.email = 'Password field is required';
+  errors.password = 'Password field is required';
+}
+if(!Validator.isLength(data.password, {min: 6, max: 30})) {
+  errors.password = 'Password must be at least 6 characters';
+}
+if(Validator.isEmpty(data.password2)){
+  errors.password2 = 'Confirm Password field is required';
+}
+if(!Validator.equals(data.password, data.password2)){
+  errors.password2 = 'Passwords must match';
 }
 
   return {
