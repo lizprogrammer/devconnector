d21e4770939564f090a58601642deb1f493c34ac
Elizabeth Rosso <lizprogrammer@yahoo.com>
2018-12-12 22:12:53 -0500

    code

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -24,6 +24,7 @@ router.get('/test', (req, res) => res.json({
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
   const errors = {};
   Profile.findOne({ user: req.user.id })
+    .populate('user', ['name', 'avatar'])
     .then(profile => {
       if (!profile) {
         errors.noprofile = 'There is no profile for this user';
@@ -71,7 +72,8 @@ passport.authenticate('jwt', { session: false }),
     if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
     if (req.body.instagram) profileFields.social.instagram = req.body.handle.instagram;
 
-    Profile.findOne({ user: req.user.id }).then(profile => {
+    Profile.findOne({ user: req.user.id })
+    .then(profile => {
       if (profile) {
         // Update
         Profile.findOneAndUpdate(
