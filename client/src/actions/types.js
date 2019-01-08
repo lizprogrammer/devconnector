380fe3337f11107ec76f895bb1807e800b2a92bd
Elizabeth Rosso <lizprogrammer@yahoo.com>
2019-01-08 00:33:45 -0500

    Login successful

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,5 @@
 export const GET_ERRORS = 'GET_ERRORS';
 // export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 // export const GET_PROFILE = 'GET_PROFILE';
-// export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
 
